172cfec94b4bd4e198d552ed1a894fd9bb124a92
DawDok <dawiddoktor91@gmail.com>
2025-01-06 16:31:03 +0100

    https://github.com/DawKul/Aplikacja-mobilna.git Poprawa logowania i rejestracja

--- Aplikacja Treningowa/Aplikacja Treningowa/obj/Debug/net8.0-android/android/src/crc64fbf84d674e0ef274/MainActivity.java
+++ Aplikacja Treningowa/Aplikacja Treningowa/obj/Debug/net8.0-android/android/src/crc64fbf84d674e0ef274/MainActivity.java
@@ -4,12 +4,18 @@ package crc64fbf84d674e0ef274;
 public class MainActivity
 	extends crc6488302ad6e9e4df1a.MauiAppCompatActivity
 	implements
-		mono.android.IGCUserPeer
+		mono.android.IGCUserPeer,
+		android.hardware.SensorEventListener
 {
 /** @hide */
 	public static final String __md_methods;
 	static {
 		__md_methods = 
+			"n_onCreate:(Landroid/os/Bundle;)V:GetOnCreate_Landroid_os_Bundle_Handler\n" +
+			"n_onResume:()V:GetOnResumeHandler\n" +
+			"n_onPause:()V:GetOnPauseHandler\n" +
+			"n_onAccuracyChanged:(Landroid/hardware/Sensor;I)V:GetOnAccuracyChanged_Landroid_hardware_Sensor_IHandler:Android.Hardware.ISensorEventListenerInvoker, Mono.Android, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null\n" +
+			"n_onSensorChanged:(Landroid/hardware/SensorEvent;)V:GetOnSensorChanged_Landroid_hardware_SensorEvent_Handler:Android.Hardware.ISensorEventListenerInvoker, Mono.Android, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null\n" +
 			"";
 		mono.android.Runtime.register ("Aplikacja_Treningowa.MainActivity, Aplikacja Treningowa", MainActivity.class, __md_methods);
 	}
@@ -31,6 +37,46 @@ public class MainActivity
 			mono.android.TypeManager.Activate ("Aplikacja_Treningowa.MainActivity, Aplikacja Treningowa", "System.Int32, System.Private.CoreLib", this, new java.lang.Object[] { p0 });
 		}
 	}
+
+
+	public void onCreate (android.os.Bundle p0)
+	{
+		n_onCreate (p0);
+	}
+
+	private native void n_onCreate (android.os.Bundle p0);
+
+
+	public void onResume ()
+	{
+		n_onResume ();
+	}
+
+	private native void n_onResume ();
+
+
+	public void onPause ()
+	{
+		n_onPause ();
+	}
+
+	private native void n_onPause ();
+
+
+	public void onAccuracyChanged (android.hardware.Sensor p0, int p1)
+	{
+		n_onAccuracyChanged (p0, p1);
+	}
+
+	private native void n_onAccuracyChanged (android.hardware.Sensor p0, int p1);
+
+
+	public void onSensorChanged (android.hardware.SensorEvent p0)
+	{
+		n_onSensorChanged (p0);
+	}
+
+	private native void n_onSensorChanged (android.hardware.SensorEvent p0);
 
 	private java.util.ArrayList refList;
 	public void monodroidAddReference (java.lang.Object obj)
